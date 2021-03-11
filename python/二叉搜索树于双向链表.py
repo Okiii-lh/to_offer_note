0a53e2553b75592bfef20eeebb0156a6d4d09089
liuhe_tech <13132515202@163.com>
2021-03-11 09:06:22 +0800

    0311

---
+++ python/二叉搜索树于双向链表.py
@@ -0,0 +1,26 @@
+# encoding=utf-8
+# 
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def tree_to_double_list(root):
+	def dfs(cur):
+		if not cur: return None
+		dfs(cur.left)
+
+		if pre:
+			pre.right, cur.left = cur, self.pre
+		else:
+			head = pre
+		pre = cur
+
+		dfs(cur.right)
+
+	if not root: return None
+	pre = None
+	dfs(root)
+	head.left, pre.right = pre, head
+	return head
--- python/复杂链表的复制.py
+++ python/复杂链表的复制.py
@@ -1,3 +1,49 @@
 # encoding=utf-8
 # 
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
 
+def copy_random_list(head):
+	def dfs(head):
+		if not head: return None
+		if head in visited:
+			return visited[head]
+		copy = Node(head.val, None, None)
+		visited[head] = copy
+		copy.next = dfs(head.next)
+		copy.random = dfs(head.random)
+		return copy
+
+	visted = {}
+	return dfs(head)
+
+def copy_random_list2(head):
+	if not head: return None
+
+	now = head
+
+	while(head):
+		new_node = Node(now.val, now.next, None)
+		new_node.next = now.next
+		now.next = new_node
+	now = head
+
+	while(now):
+		if now.random:
+			now.next.random = now.random
+		now = now.next.next
+
+	old_head = head
+	new_head = head.next
+
+	while old_head:
+		old_head.next = old_head.next.next
+		if new_head.next:
+			new_head.next = new_head.next.next
+		old_head = old_head.next
+		new_head = new_head.next
+
+	return new_head
