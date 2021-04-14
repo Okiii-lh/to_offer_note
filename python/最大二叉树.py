7b23e93bf0e1cc4a293cff84f837d1a15bf3ef83
liuhe_tech <13132515202@163.com>
2021-04-14 10:23:06 +0800

    0414

--- python/最大二叉树.py
+++ python/最大二叉树.py
@@ -4,3 +4,37 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
+
+def constructTree(nums):
+	if len(nums) == 0:
+		return None
+
+	max_index = 0
+	max_value = 0
+
+	for i in len(nums):
+		if nums[i] >= max_value:
+			max_index = i
+			max_value = nums[i]
+
+	root = TreeNode(max_value)
+	root.left = constructTree(nums[0:max_index])
+	root.right = constructTree(nums[max_index:])
+
+	return root
+
+
+def constructTree2(nums):
+	if nums is None:
+		return None
+
+	max_value = max(nums)
+	max_index = nums.index(max_value)
+
+	root = TreeNode(max_value)
+	root.left = constructTree2(nums[:max_index])
+	root.right = constructTree2(nums[max_index:])
+
+	return root
