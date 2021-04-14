e7995afc4153ad73592974cb397ed4111438badc
liuhe_tech <13132515202@163.com>
2021-04-14 09:58:47 +0800

    0414

--- python/填充每个节点的下一个右侧节点指针.py
+++ python/填充每个节点的下一个右侧节点指针.py
@@ -5,3 +5,24 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
+def connect(root):
+	if root is None:
+		return root
+
+	connectTwo(root.left, root.right)
+
+	return root
+
+
+def connectTwo(node1, node2):
+	if node1 is None or node2 is None:
+		return
+
+
+	node1.next = node2
+
+	connectTwo(node1.left, node1.right)
+	connectTwo(node1.right, node2.left)
+	connectTwo(node2.left, node2.right)
